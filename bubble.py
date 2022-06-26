acdb1f2c29739c47703f5d61d36c43dcb90a98bb
SimonMekonnen <simonmekonnen80@gmail.com>
2022-06-26 03:03:40 -0800

    bubble

---
+++ bubble.py
@@ -0,0 +1,10 @@
+def countSwaps(a):
+    b=0
+    for i in range (len(a)):
+        for j in range (len(a)-1):
+            if a[j] > a[j+1]:
+                a[j],a[j+1] = a[j+1],a[j]
+                b+=1
+    print("Array is sorted in {} swaps.".format(b))
+    print("First Element: {}".format(a[0]))
+    print("Last Element: {}".format(a[len(a)-1]))
