39f813985ec239dc4d558bf4209081a1a6da5510
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-10-09 13:42:16 -0800

    Time: 263 ms (13.47%), Space: 15.1 MB (73.25%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.py
+++ 735-asteroid-collision/735-asteroid-collision.py
@@ -3,10 +3,7 @@ class Solution:
         des_ast = []
         astroid = 0
         while astroid < len(asteroids):
-            if not des_ast:
-                des_ast.append(asteroids[astroid])
-                astroid+=1
-            elif des_ast[-1] > 0 and asteroids[astroid] < 0:
+            if des_ast and (des_ast[-1] > 0 and asteroids[astroid] < 0):
                 if abs(des_ast[-1]) > abs(asteroids[astroid]):
                     astroid+=1
                 elif abs(des_ast[-1]) == abs(asteroids[astroid]):
