08afdfb214daf757820cde4c5b92c2df370d15c3
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-10-14 09:00:51 -0800

    Time: 679 ms (37.40%), Space: 16.5 MB (17.25%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -1,25 +1,27 @@
 class Solution:
     def maxSatisfied(self, cus: List[int], g: List[int], m: int) -> int:
         ans = 0
+        sat = 0
         total = 0
         left , right = 0 , 0
         for right in range(m):
             if g[right]==1:
                 ans+=cus[right]
+            else:
+                sat+=cus[right]
             total = max(ans,total)
         right+=1
         while right < len(cus):
             if g[right] == 1:
                 ans+=cus[right]
+            else:
+                sat+=cus[right]
             if g[left] == 1:
                 ans-=cus[left]
             total = max(ans,total)
             right+=1
             left+=1
-        for i,n in enumerate(cus):
-            if g[i] == 0:
-                total+=n
-        return total
+        return total + sat
         
             
             
