2c8418d8aae81eba4ad9d40dd911bacb6ffc62d1
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-10-16 08:48:03 -0800

    Time: 236 ms (60.32%), Space: 13.9 MB (93.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,11 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         left = 0
-        ans = 0
+        ans,_max = 0,0
         dic = defaultdict(int)
         for right in range(0,len(s)):
             dic[s[right]]+=1
-            while ((right - left +1) - max(dic.values())) > k:
+            _max = max(_max,dic[s[right]])
+            while (right - left +1) - _max > k:
                 dic[s[left]]-=1
                 left+=1
             ans = max(ans,right - left + 1)
