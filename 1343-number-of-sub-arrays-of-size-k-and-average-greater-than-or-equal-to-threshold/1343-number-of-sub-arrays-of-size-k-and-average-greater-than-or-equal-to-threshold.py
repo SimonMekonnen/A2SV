c0f2adfcc0f7bf872a1af8239d0d5fc19ff7e829
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-10-19 12:47:37 -0800

    Time: 683 ms (86.87%), Space: 27.3 MB (23.73%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,19 +1,31 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         right,left,totalsum,ans= 0,0,0,0
-        for i in range(k):
+        # for i in range(k):
+        #     totalsum+=arr[right]
+        #     right+=1
+        # while right<=len(arr):
+        #     if totalsum/k>=threshold:
+        #         ans+=1
+        #     if right ==  len(arr):
+        #         break
+        #     totalsum+=arr[right]
+        #     totalsum-=arr[left]
+        #     left+=1
+        #     right+=1
+        # return ans
+        for right in range(len(arr)):
+            if right - left + 1 > k:
+                if totalsum / k >= threshold:
+                    ans+=1
+                totalsum-=arr[left]
+                left+=1
             totalsum+=arr[right]
-            right+=1
-        while right<=len(arr):
-            if totalsum/k>=threshold:
-                ans+=1
-            if right ==  len(arr):
-                break
-            totalsum+=arr[right]
-            totalsum-=arr[left]
-            left+=1
-            right+=1
+        c = totalsum/k
+        ans+=c>=threshold
         return ans
+            
+            
     
 #time complexity O(n)
 #space complexity O(n)
