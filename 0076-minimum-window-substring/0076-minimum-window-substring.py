751a38e6fba53fa717c26306f96bf81a907bd494
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-10-22 12:38:07 -0800

    Time: 1264 ms (6.04%), Space: 14.8 MB (37.01%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def contains(sc,tc):
+            for i in tc:
+                if i not in sc or sc[i] < tc[i]:
+                    return False
+            return True
+        Tcount = Counter(t)
+        Scount = defaultdict(int)
+        left = 0
+        right = 0
+        ans = math.inf
+        l = 0
+        r = 0
+        if len(s)<len(t):
+            return ""
+        while right <= len(s):
+            b = contains(Scount,Tcount)
+            if b == False:
+                if right!= len(s):
+                    Scount[s[right]]+=1
+                right+=1
+            else:
+                if right - left < ans:
+                    l = left
+                    r = right
+                    ans = right - left
+                Scount[s[left]]-=1
+                left+=1
+        return s[l:r]
+# time complexity O(2*26*n) -> O(n)
+# space complexity O(26) ->O(1)
+    
+
+                
+        
+       
+        
