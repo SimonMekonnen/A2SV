867ca6e345b0377093063ae1e678fa90e02ae8cb
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-10-22 12:43:11 -0800

    Time: 144 ms (79.86%), Space: 14.6 MB (83.90%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,38 +1,54 @@
 class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        def contains(sc,tc):
-            for i in tc:
-                if i not in sc or sc[i] < tc[i]:
-                    return False
-            return True
-        Tcount = Counter(t)
-        Scount = defaultdict(int)
-        left = 0
-        right = 0
-        ans = math.inf
-        l = 0
-        r = 0
-        if len(s)<len(t):
-            return ""
-        while right <= len(s):
-            b = contains(Scount,Tcount)
-            if b == False:
-                if right!= len(s):
-                    Scount[s[right]]+=1
-                right+=1
-            else:
-                if right - left < ans:
-                    l = left
-                    r = right
-                    ans = right - left
-                Scount[s[left]]-=1
-                left+=1
-        return s[l:r]
-# time complexity O(2*26*n) -> O(n)
-# space complexity O(26) ->O(1)
-    
+    def minWindow(self, s, t):
 
-                
-        
-       
-        
+        if not t or not s:
+            return ""
+
+        # Dictionary which keeps a count of all the unique characters in t.
+        dict_t = Counter(t)
+
+        # Number of unique characters in t, which need to be present in the desired window.
+        required = len(dict_t)
+
+        # left and right pointer
+        l, r = 0, 0
+
+        # formed is used to keep track of how many unique characters in t are present in the current window in its desired frequency.
+        # e.g. if t is "AABC" then the window must have two A's, one B and one C. Thus formed would be = 3 when all these conditions are met.
+        formed = 0
+
+        # Dictionary which keeps a count of all the unique characters in the current window.
+        window_counts = {}
+
+        # ans tuple of the form (window length, left, right)
+        ans = float("inf"), None, None
+
+        while r < len(s):
+
+            # Add one character from the right to the window
+            character = s[r]
+            window_counts[character] = window_counts.get(character, 0) + 1
+
+            # If the frequency of the current character added equals to the desired count in t then increment the formed count by 1.
+            if character in dict_t and window_counts[character] == dict_t[character]:
+                formed += 1
+
+            # Try and contract the window till the point where it ceases to be 'desirable'.
+            while l <= r and formed == required:
+                character = s[l]
+
+                # Save the smallest window until now.
+                if r - l + 1 < ans[0]:
+                    ans = (r - l + 1, l, r)
+
+                # The character at the position pointed by the `left` pointer is no longer a part of the window.
+                window_counts[character] -= 1
+                if character in dict_t and window_counts[character] < dict_t[character]:
+                    formed -= 1
+
+                # Move the left pointer ahead, this would help to look for a new window.
+                l += 1    
+
+            # Keep expanding the window once we are done contracting.
+            r += 1    
+        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
