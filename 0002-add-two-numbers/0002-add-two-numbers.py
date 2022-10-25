3b224a170c6cca8c1335d4b1de88bddc41c87575
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-10-25 12:02:26 -0800

    Time: 79 ms (84.40%), Space: 13.9 MB (86.15%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -7,34 +7,15 @@ class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         c = 0
         cur = dummy = ListNode()
-        while l1 and l2:
-            s = l1.val + l2.val + c
-            if s > 9:
-                c = 1
-            else:
-                c = 0
-            cur.next = ListNode(val = s%10)
+        while l1 or l2:
+            l1v = l1.val if l1 else 0
+            l2v = l2.val if l2 else 0
+            s = l1v + l2v + c
+            cur.next = ListNode(val = s % 10)
             cur = cur.next
-            l1 = l1.next
-            l2 = l2.next
-        while l1:
-            s = l1.val + c
-            if s > 9:
-                c = 1
-            else:
-                c = 0
-            cur.next = ListNode(val = s%10)
-            cur = cur.next
-            l1 = l1.next
-        while l2:
-            s = l2.val + c
-            if s > 9:
-                c = 1
-            else:
-                c = 0
-            cur.next = ListNode(val = s%10)
-            cur = cur.next
-            l2 = l2.next
+            c = 1 if s > 9 else 0
+            l1 = l1.next if l1 else l1
+            l2 = l2.next if l2 else l2
         if c == 1:
             cur.next = ListNode(val = 1)  
         return dummy.next
