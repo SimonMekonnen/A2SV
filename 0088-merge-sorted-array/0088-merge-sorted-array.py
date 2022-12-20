66b6261020eae547f63b4282da5de7b526517770
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-12-20 08:43:52 -0800

    Time: 88 ms (9.22%), Space: 13.9 MB (38.78%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,20 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        l = m
-        r = n 
-        k =  m + n
-        nums1.insert(0,-math.inf)
-        nums2.insert(0,-math.inf)
-        while k >= 0:
-            if nums1[l] > nums2[r]:
+        l = m - 1
+        r = n - 1
+        k =  m + n - 1
+        while r >= 0:
+            if l >= 0 and nums1[l] > nums2[r]:
                 nums1[k] = nums1[l]
                 l-=1
             else:
                 nums1[k] = nums2[r]
                 r-=1
             k-=1
-        nums1.pop(0)
             
                 
      
