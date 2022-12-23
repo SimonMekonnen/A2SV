09b1c19d97a2a714124e121c006d09b63901ba2d
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2022-12-23 07:54:21 -0800

    Time: 43 ms (80.73%), Space: 13.9 MB (83.48%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -5,18 +5,18 @@ class Solution:
         for i in range(len(order)):
             dic[order[i]] = i
         
-        for h in range(len(words) - 1):
+        for h in range(1,len(words)):
             p1 = 0
             Flag = True
-            while p1 < len(words[h]) and p1 < len(words[h + 1]):
-                if dic[words[h][p1]] > dic[words[h + 1][p1]]:
+            while p1 < len(words[h]) and p1 < len(words[h - 1]):
+                if dic[words[h - 1][p1]] > dic[words[h][p1]]:
                     return False
-                if dic[words[h][p1]] < dic[words[h + 1][p1]]:
+                if dic[words[h - 1][p1]] < dic[words[h][p1]]:
                     Flag = False
                     break
                 p1+=1
                 
-            if Flag and len(words[h]) > len(words[h + 1]):
+            if Flag and len(words[h - 1]) > len(words[h]):
                 return False
         return True
                     
