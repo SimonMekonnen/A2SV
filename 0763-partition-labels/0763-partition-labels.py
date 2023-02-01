d397d8d8e9b5832d633b475623716fd9534c616d
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-02-01 15:56:47 -0800

    Time: 45 ms (60.18%), Space: 13.8 MB (60.53%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,21 +1,42 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         
-        count  = Counter(s)
-        new = defaultdict(int)
-        ans = []
-        
-        for i in s:
-            new[i]+=1
-            Flag = True
-            for j in new:
-                if new[j] != count[j]:
-                    Flag = False
-            if Flag:
-                ans.append(sum(new.values()))
-                new.clear()
-
+        dic = {}
+        for i in range(len(s)):
+            dic[s[i]] = i
+        ans = []  
+        left = 0
+        right = dic[s[0]]
+        i = 0
+        while i < len(s):
+            
+            right = max(dic[s[i]],right)
+            if i == right:
+         
+                ans.append(right - left + 1)
+                left = right + 1
+                
+    
+            i+=1
+            
         return ans
+                
+        
+#         count  = Counter(s)
+#         new = defaultdict(int)
+#         ans = []
+        
+#         for i in s:
+#             new[i]+=1
+#             Flag = True
+#             for j in new:
+#                 if new[j] != count[j]:
+#                     Flag = False
+#             if Flag:
+#                 ans.append(sum(new.values()))
+#                 new.clear()
+
+#         return ans
         
         
         
