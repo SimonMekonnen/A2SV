2921705c9921726c921224a201819a98801cccaa
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-02-12 12:25:04 -0800

    Time: 472 ms (67.04%), Space: 20.1 MB (73.90%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+        
+        score.sort(key = lambda x : -x[k])
+        
+        return score
+        
