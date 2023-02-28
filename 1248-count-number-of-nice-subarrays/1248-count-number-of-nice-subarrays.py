d35808c59d7e26cbfa85d38c0e7b107a94c1e94f
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-02-28 04:11:59 -0800

    Time: 934 ms (39.98%), Space: 21 MB (47.19%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -10,8 +10,10 @@ class Solution:
                 left += 1
                 
             ans += right - left + 1
+            
         n = len(nums)
         oddCount = ano = left = 0
+        
         for right in range(n):
             oddCount += nums[right] % 2
             
