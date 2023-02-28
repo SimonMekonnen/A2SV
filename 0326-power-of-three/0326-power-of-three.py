3ab1fdc9331bf3898bd371f19fca538c5b95ebbf
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-02-27 23:36:18 -0800

    Time: 113 ms (29.32%), Space: 13.8 MB (52.15%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -2,7 +2,7 @@ class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         if n == 1:
             return True
-        elif n%3!=0 or n==0:
+        elif  n < 1:
             return False
         else:
             return self.isPowerOfThree(n/3)
