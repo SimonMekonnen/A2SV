dc6d945ff11f8ee8b9430113586c3616a02caaa7
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-02-28 23:56:15 -0800

    Time: 688 ms (12.62%), Space: 44.2 MB (13.00%) - LeetHub

--- 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
@@ -1,36 +1,36 @@
 class DataStream:
 
     def __init__(self, value: int, k: int):
+        self.dic = defaultdict(int)  
+        self.val = value
+        self.length = k
         self.arr = []
-        self.value = value
-        self.k = k
-        self.dic = defaultdict(int)
-        self.count = 0
+        self.right = 0
         self.left = 0
-
     def consec(self, num: int) -> bool:
         self.arr.append(num)
-        self.count += 1
-        if self.count > self.k:
-            
-            self.dic[self.arr[self.left]] -= 1
-            
-            if  self.dic[self.arr[self.left]] <= 0:
-                del  self.dic[self.arr[self.left]]
-            
-            self.left+=1
-    
-        self.dic[num] += 1
-        if len(self.dic) != 1 or self.dic[self.value] != self.k:
-            
-            return False
+        if self.right - self.left + 1 <= self.length:
+            self.right += 1 
+            self.dic[num] += 1
+            if len(self.dic) == 1 and self.dic[self.val] == self.length:
+                return True
         else:
-            return True 
-            
-        
-        
-        
+            self.dic[self.arr[self.left]] -= 1
+            if self.dic[self.arr[self.left]] ==  0:
+                 del self.dic[self.arr[self.left]]
 
+            self.dic[num] += 1
+            self.left += 1
+            self.right += 1
+            if len(self.dic) == 1 and self.val in self.dic:
+                return True
+    
+                
+            
+            
+    
+        
+    
 
 # Your DataStream object will be instantiated and called as such:
 # obj = DataStream(value, k)
