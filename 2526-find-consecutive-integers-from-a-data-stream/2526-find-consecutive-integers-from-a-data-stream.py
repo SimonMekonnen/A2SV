f29045a5c07b77dff1dd5e864d3175b280f5f5dd
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-03-01 00:03:59 -0800

    Time: 547 ms (81.84%), Space: 43.9 MB (18.37%) - LeetHub

--- 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
+++ 2526-find-consecutive-integers-from-a-data-stream/2526-find-consecutive-integers-from-a-data-stream.py
@@ -1,36 +1,22 @@
 class DataStream:
 
     def __init__(self, value: int, k: int):
-        self.dic = defaultdict(int)  
+        self.stk = deque()
         self.val = value
-        self.length = k
-        self.arr = []
-        self.right = 0
-        self.left = 0
-    def consec(self, num: int) -> bool:
-        self.arr.append(num)
-        if self.right - self.left + 1 <= self.length:
-            self.right += 1 
-            self.dic[num] += 1
-            if len(self.dic) == 1 and self.dic[self.val] == self.length:
-                return True
-        else:
-            self.dic[self.arr[self.left]] -= 1
-            if self.dic[self.arr[self.left]] ==  0:
-                 del self.dic[self.arr[self.left]]
+        self.k = k
 
-            self.dic[num] += 1
-            self.left += 1
-            self.right += 1
-            if len(self.dic) == 1 and self.val in self.dic:
-                return True
-    
-                
-            
-            
-    
+    def consec(self, num: int) -> bool:
         
-    
+        if num != self.val:
+            self.stk = deque()
+            return False
+        self.stk.append(num)
+        if len(self.stk) >= self.k:
+            return True
+        
+        
+        
+
 
 # Your DataStream object will be instantiated and called as such:
 # obj = DataStream(value, k)
