a726bde381dc5a62daac5eaa4c7580f21dbf95b8
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-03-01 06:27:59 -0800

    Time: 35 ms (49.45%), Space: 13.9 MB (51.78%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,18 +1,11 @@
+
 class Solution:
     def getRow(self, ri: int) -> List[int]:
         
-        if ri == 0:
-            return [1]
+        return [(math.factorial(ri)//((math.factorial(i) * math.factorial(ri - i)))) for i in range(ri + 1)]
         
-        else:
-            b = self.getRow(ri - 1)
-            c = [1]
-            for i in range(len(b) - 1):
-                c.append(b[i] + b[i + 1])
-            c.append(1)
-            
-            return  c
         
+       
         
         
     
