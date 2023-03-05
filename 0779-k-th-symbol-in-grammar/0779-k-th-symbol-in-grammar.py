1e64262afb8ca7f120bad2362530c4daad3a5218
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-03-05 10:29:41 -0800

    Time: 24 ms (96.40%), Space: 13.9 MB (54.27%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,15 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        
+        if n == 1:
+            return 0
+        
+        l = 2 ** (n - 1)
+        
+        if k > l//2: 
+            return 1 - self.kthGrammar(n - 1,k - l//2)
+        else:
+            return self.kthGrammar(n - 1,k)
+        
+        
+        
