64c93a5542ef7811e180caf9711bf77483765ff5
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-03-12 06:41:59 -0800

    Time: 70 ms (17.60%), Space: 15.1 MB (67.95%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -5,24 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def helper(self,root):
+    def helper(self,root,count):
         if not root:
             return [] 
         
-        left = self.helper(root.left)
-        right = self.helper(root.right)
+        left = self.helper(root.left,count)
+        right = self.helper(root.right,count)
         if (sum(left) + sum(right) + root.val) // (len(left) + len(right) + 1) == root.val:
-            self.count += 1
+            count[0] += 1
         
         return left + [root.val] + right
         
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
         
-        self.count = 0
+        count = [0]
         
-        self.helper(root)
+        self.helper(root,count)
         
-        return self.count
+        return count[0]
         
     
         
