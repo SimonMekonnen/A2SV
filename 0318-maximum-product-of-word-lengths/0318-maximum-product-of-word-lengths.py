7ea9ee4305443741784336d2fec1152a2538100e
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-04-04 08:40:50 -0800

    Time: 7506 ms (5.53%), Space: 17.3 MB (75.74%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -1,17 +1,23 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        
-        b = [set(i) for i in words]
+      
+        today = []
+        for i in words:
+            rep = ["0" for i in range(26)]
+            for j in i:
+                rep[ord(j) - ord("a")] = "1"
+
+            today.append(int("".join(rep),2))
         ans = 0
         for i in range(len(words)):
-            for j in range(i + 1,len(words)):
-                flag = True
-                for k in b[i]:
-                    
-                    if k in b[j]:
-                        flag = False
-                if flag:
-                    ans = max(len(words[i]) * len(words[j]), ans)
+            for j in range(i,len(words)):
+                if today[i] & today[j] == 0:
+                    ans = max(ans,len(words[i] * len(words[j])))
         return ans
-                
+        
+            
+        
+        
+        
+      
         
