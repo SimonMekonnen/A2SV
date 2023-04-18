45c78cf72ea456f0d5651fd82fdf1b1e84f09dd5
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-04-18 10:24:08 -0800

    Time: 27 ms (90.33%), Space: 13.8 MB (55.13%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -3,15 +3,19 @@ class Solution:
         ans = ""
         l = 0
         r = 0
+        
         for i in range((len(wo1)+ len(wo2))):
             if l < len(wo1) and i%2 == 0:
                 ans+=wo1[l]
                 l+=1
+                
             elif r < len(wo2) and i%2 == 1:
                 ans+=wo2[r]
                 r+=1
+                
         ans += wo1[l:] if l  < len(wo1) else ""
         ans += wo2[r:] if r < len(wo2) else ""
+        
         return ans
                        
         
