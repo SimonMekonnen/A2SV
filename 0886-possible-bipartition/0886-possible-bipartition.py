fd606e9ae2b5f8c70bfe94b8da6a2e9a5c6733b9
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-04 13:38:57 -0800

    Time: 782 ms (19.24%), Space: 22.9 MB (14.44%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -6,18 +6,19 @@ class Solution:
             dic[i[1]].add(i[0])
         visited = set()
         for i in range(1,n + 1):
-            color = [2 for i in range(n + 1)]
-            stk = [[i,1]]
-            visited.add((i,1))
-            while stk:
-                now = stk.pop()
-                if color[now[0]] != 2:
-                    return False
-                color[now[0]] = now[1]
-                for i in dic[now[0]]:
-                    if (i,1 - now[1]) not in visited:
-                        stk.append([i,1 - now[1]])
-                        visited.add((i,1 - now[1]))
+            if (i,1) not in visited:
+                color = [2 for i in range(n + 1)]
+                stk = [[i,1]]
+                visited.add((i,1))
+                while stk:
+                    now = stk.pop()
+                    if color[now[0]] != 2:
+                        return False
+                    color[now[0]] = now[1]
+                    for i in dic[now[0]]:
+                        if (i,1 - now[1]) not in visited:
+                            stk.append([i,1 - now[1]])
+                            visited.add((i,1 - now[1]))
         return True
                 
                 
