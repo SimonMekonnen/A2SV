b4729ea5a4f9bcac68879048f57f5a93ae8ef58a
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-11 06:59:17 -0800

    Time: 120 ms (16.93%), Space: 18 MB (19.63%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -8,22 +8,24 @@ class Solution:
             depend[d] += 1
         
         que = deque()
+        visited = set()
         for i,n in enumerate(depend):
             if n == 0:
                 que.append(i)
-                visited = set()
+                visited.add(i)
+                
         
-        ans = []
+
         while que:
             cur = que.popleft()
-            ans.append(cur)
+            visited.add(cur)
             for ne in graph[cur]:
                 depend[ne] -= 1
-                if depend[ne] == 0 and (ne not in visited):
+                if depend[ne] == 0:
                     que.append(ne)
                     visited.add(ne)
         
-        if len(ans) == numCourses:
+        if len(visited) == numCourses:
             return True
         return False
             
