c20c7c2c5a41f72e7f1727ca0cd848a8b9d9fef2
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-19 07:35:52 -0800

    Time: 1977 ms (8.29%), Space: 17.3 MB (18.50%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -3,16 +3,6 @@ class Solution:
         edges = []
         parent = {}
         ans = defaultdict(int)
-        for i in range(len(stones)):
-            parent[tuple(stones[i])] = tuple(stones[i])
-            for j in range(i + 1,len(stones)):
-                x1 = stones[i][0]
-                x2 = stones[i][1]
-                y1 = stones[j][0]
-                y2 = stones[j][1]
-                if x1 == y1 or x2 == y2:
-                    edges.append((tuple(stones[i]),tuple(stones[j])))
-
         def find(x):
             cur = x
             while cur != parent[cur]:
@@ -28,10 +18,19 @@ class Solution:
         def union(x,y):
             xparent = find(x)
             yparent = find(y)
-            parent[xparent] = yparent
-    
-        for x,y in edges:
-            union(x,y)
+            parent[xparent] = yparent  
+        for i in stones:
+            parent[tuple(i)] = tuple(i)
+        for i in range(len(stones)):
+            for j in range(i + 1,len(stones)):
+                x1 = stones[i][0]
+                x2 = stones[i][1]
+                y1 = stones[j][0]
+                y2 = stones[j][1]
+                if x1 == y1 or x2 == y2:
+                    union(tuple(stones[i]),tuple(stones[j]))
+
+             
         
         for i in parent:
             parent[i] = find(i)
