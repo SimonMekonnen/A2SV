fe6840439bedada6b4536a0ade6b5dd4669848d8
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-19 07:13:37 -0800

    Time: 2001 ms (7.92%), Space: 17.4 MB (17.32%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,54 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        edges = []
+        parent = {}
+        ans = defaultdict(int)
+        for i in range(len(stones)):
+            parent[tuple(stones[i])] = tuple(stones[i])
+            for j in range(i + 1,len(stones)):
+                x1 = stones[i][0]
+                x2 = stones[i][1]
+                y1 = stones[j][0]
+                y2 = stones[j][1]
+                if x1 == y1 or x2 == y2:
+                    edges.append((tuple(stones[i]),tuple(stones[j])))
+
+        def find(x):
+            cur = x
+            while cur != parent[cur]:
+                cur = parent[cur]
+            
+            while x != parent[x]:
+                 n = parent[x]
+                 parent[x] = cur
+                 x = n
+
+            return cur
+ 
+        def union(x,y):
+            xparent = find(x)
+            yparent = find(y)
+            parent[xparent] = yparent
+    
+        for x,y in edges:
+            union(x,y)
+        
+        for i in parent:
+            parent[i] = find(i)
+        
+        for i in parent:
+            ans[parent[i]] += 1
+        res = 0
+        for i in ans:
+            res += (ans[i] - 1)
+        print(parent)
+        return res
+            
+        
+        
+            
+            
+        
+        
+     
+   
