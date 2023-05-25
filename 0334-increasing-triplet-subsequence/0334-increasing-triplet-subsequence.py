37ee9ad36943c230f64778457865ca295dbe72e2
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-25 01:21:27 -0800

    Time: 1513 ms (6.17%), Space: 36.9 MB (16.65%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -6,9 +6,8 @@ class Solution:
         for i in range(1,len(nums)):
             arr1.append(min(arr1[-1],nums[i]))
             arr2.append(max(arr2[-1] ,nums[len(nums) - 1 - i]))
-        arr2.reverse()
         for i in range(len(nums)):
-            if arr1[i] < nums[i] < arr2[i]:
+            if arr1[i] < nums[i] < arr2[len(nums) - 1 - i]:
                 return True
         return False
         
