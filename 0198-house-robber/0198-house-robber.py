aa6a8d27001776ca8eb2c50a506377345ff5a3dd
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-26 07:22:27 -0800

    Time: 46 ms (31.91%), Space: 16.5 MB (13.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def dp(index ,memo = {}):
-            if index in memo:
-                return memo[index]
+        @cache
+        def dp(index):
             if index >= len(nums):
                 return 0
-            memo[index] = nums[index] + max(dp(index + 2),dp(index + 3))
-            return memo[index]
+            return nums[index] + max(dp(index + 2),dp(index + 3))
+         
         
         return max(dp(0),dp(1))
         
