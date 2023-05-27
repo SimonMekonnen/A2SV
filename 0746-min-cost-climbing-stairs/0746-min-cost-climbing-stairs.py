5674910c889ec3fdbad2afaf7a019d4752978237
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-26 20:48:36 -0800

    Time: 91 ms (5.08%), Space: 18.7 MB (5.28%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        @cache
+        def dp(index):
+            if index >= len(cost):
+                return 0
+            
+            return cost[index] + min(dp(index + 1),dp(index + 2))  
+
+        return min(dp(0),dp(1))
+        
