c978b095da18fad7764bc9812318116d96894dec
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-28 13:06:00 -0800

    Time: 47 ms (36.88%), Space: 16.3 MB (10.18%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,14 +1,20 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        dp = [0,0,1,0]
-        
+        first = 0
+        second = 1
+        if n == 1:
+            return 0
         for i in range(2,n + 1):
             if i % 2 == 1:
-                dp.append(1 - dp[i - 1])
+                c = second
+                first = second
+                second = 1 - c    
             else:
-                if dp[i - 1] * dp[i - 2] == 0:
-                    dp.append(1)
+                if first * second == 0:
+                    first = second
+                    second = 1
                 else:
-                    dp.append(0)
-        return dp[n]
+                    first = second
+                    second = 0 
+        return second
         
