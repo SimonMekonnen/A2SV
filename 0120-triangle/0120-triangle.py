df63ac7567ee286152ae67e053c4c0d1e0ed4d2e
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-05-31 23:09:26 -0800

    Time: 96 ms (6.30%), Space: 17.7 MB (13.94%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,10 +1,12 @@
 class Solution:
-    def minimumTotal(self, tr: List[List[int]]) -> int:  
-        @cache
-        def dp(row,col):
-            if row >= len(tr):
-                return 0
-            return tr[row][col] + min(dp(row + 1,col),dp(row + 1,col + 1))
-    
-        return dp(0,0)
+    def minimumTotal(self, tri: List[List[int]]) -> int:
+        dp = [[inf for i in range(len(tri))] for i in range(len(tri))]
+        dp[0][0] = tri[0][0]
+        for row in range(len(dp) - 1):
+            for col in range(len(tri[row])):
+                dp[row + 1][col] = min(dp[row + 1][col],dp[row][col] + tri[row + 1][col])
+                dp[row + 1][col + 1] = min(dp[row + 1][col + 1],dp[row][col] + tri[row + 1][col + 1])
+        return min(dp[-1])
+
+            
         
