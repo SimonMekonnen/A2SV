7cb9cac7072dac5940381cb6cde504120530b462
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-06-05 05:39:55 -0800

    Time: 12 ms (78.53%), Space: 11.3 MB (99.69%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> dp(10000+1);
+        const int sz = 10001;
+        int dp[sz] = {0};
         for(int num:  nums) {
             dp[num] += num;
         }
         
         
         int ans = 0;
-        for(int i = 3; i < dp.size(); i++) {
+        for(int i = 3; i < sz; i++) {
             dp[i] += max(dp[i-2], dp[i-3]);
             ans = max(ans, dp[i]);
         }
