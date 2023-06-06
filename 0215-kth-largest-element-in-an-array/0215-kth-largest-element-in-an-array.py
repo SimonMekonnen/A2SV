4c28695defa8469bd0363e1626b468bb2dbe57e1
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-06-06 04:48:55 -0800

    Time: 581 ms (30.78%), Space: 29.9 MB (30.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,12 +4,14 @@ class Solution:
         def helper(arr,k):
             if len(arr) == 1:
                 return arr[0]
-           
-            num = arr[0]
+            index = random.randint(0,len(arr) - 1)
+            num = arr[index]
             left = []
             right = []
             
-            for i in range(1,len(arr)):
+            for i in range(0,len(arr)):
+                if i == index:
+                    continue
                 if arr[i] > num:
                     right.append(arr[i])
                 else:
