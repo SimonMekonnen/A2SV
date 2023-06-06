41d555fadb5a89ef9e6ed15f872282b9a9d6f83a
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-06-05 23:02:46 -0800

    Time: 60 ms (20.97%), Space: 16.6 MB (22.28%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,17 +3,16 @@ class Solution:
         if len(nums) == 1:
             return [nums]
         ans = set()
-        def bt(index,arr):
+        def bt(arr):
             if len(arr) == len(nums):
                 ans.add(tuple(arr))
                 return 
             
             for i in range(len(nums)):
-                if i!= index and nums[index] not in arr:
-                    bt(i,arr + [nums[index]]) 
+                if nums[i] not in arr:
+                    bt(arr + [nums[i]]) 
             
-        for i in range(len(nums)):
-            bt(i,[])
+        bt([])
         return ans
                 
         
