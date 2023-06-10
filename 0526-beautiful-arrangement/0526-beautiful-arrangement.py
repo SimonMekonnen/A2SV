d30858eaa393afc415bfbff04faae0bc67d8f56c
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-06-10 06:39:14 -0800

    Time: 3122 ms (5.15%), Space: 16.4 MB (34.33%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,18 +1,21 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
         count = 0
-        def bt(arr,visited):
+        def bt(mask):
             nonlocal count
-            if len(arr) == n:
+            if int.bit_count(mask) == n:
                 count += 1
                 return 
             
             for i in range(1,n + 1):
-                if i not in visited and\
-                (i % (len(arr) + 1) == 0 or (len(arr) + 1) % i == 0) :
-                    visited.add(i)
-                    bt(arr + [i],visited)
-                    visited.remove(i)
-        bt([],set())
+                c = 1 << (i - 1)
+                num = int.bit_count(mask)
+                if c & mask == 0 and\
+                (i % (num + 1) == 0 or \
+                 (num + 1) % i == 0) :
+                    mask |= c
+                    bt(mask)
+                    mask ^= c
+        bt(0)
         return count
     
