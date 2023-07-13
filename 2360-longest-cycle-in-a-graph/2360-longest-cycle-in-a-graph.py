6060d3408b118c81577ed61e8ca300a73e7c0e84
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-07-13 06:42:05 -0800

    Time: 1361 ms (50.41%), Space: 170.5 MB (27.10%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -0,0 +1,31 @@
+class Solution:
+    def longestCycle(self, edges: List[int]) -> int:
+        n = len(edges)
+        colors = [0 for _ in range(n)]
+        stack = {}
+        ans = 0
+        def dfs(node, i):
+            nonlocal ans
+            if colors[node] == 1:
+                ans = max(ans, i - stack[node])
+                return 
+            if colors[node] == 2:
+                return 
+            
+            stack[node] = i
+            colors[node] = 1
+            if edges[node] != -1:
+                
+                dfs(edges[node], i + 1)
+            
+            del stack[node]
+            
+            colors[node] = 2
+            return 
+        
+        for i in range(n):
+            if not colors[i]:
+                dfs(i, 0)
+        return ans if ans else -1
+                
+        
