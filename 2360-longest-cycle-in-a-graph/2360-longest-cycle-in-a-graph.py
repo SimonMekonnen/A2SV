d7a5dd5385f5ca1ffc31244567996bc1603ec604
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-07-13 20:34:54 -0800

    Time: 1419 ms (43.90%), Space: 152.8 MB (45.81%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,26 +1,50 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
-        color = [0 for i in range(len(edges))]
-        stack = {}
-        ans = -1
-        def dfs(node,length):
-            nonlocal ans
-            if color[node] == 2:
-                return 
-            if color[node] == 1:
-                ans = max(ans,length - stack[node])
-                return 
-            color[node] = 1
-            stack[node] = length
-            
-            if edges[node] != -1:
-                dfs(edges[node],length + 1)
-                
-            color[node] = 2
-            return 
+        incycle = [1 for i in range(len(edges))]
+        degree = [0 for i in range(len(edges))]
+        que = deque()
+        visited = [0 for i in range(len(edges))]
+        ans = 0
         for i in range(len(edges)):
-            if color[i] == 0:
+            if edges[i]!= -1:
+                degree[edges[i]] += 1
+         
+        for i in range(len(edges)):
+            if degree[i] == 0:
+                que.append(i)
+     
+        while que:
+            now = que.popleft()
+            incycle[now] = 0
+            if edges[now] != -1:
+                degree[edges[now]] -= 1
+                if degree[edges[now]] == 0:
+                    que.append(edges[now])
+       
+        def dfs(node,count):
+            nonlocal ans
+            if visited[node] == 1:
+                ans = max(ans,count)
+                return 
+            visited[node] = 1
+            if edges[node] != -1:
+                dfs(edges[node],count + 1)
+            
+            return
+        
+        for i in range(len(edges)):
+            if visited[i] != 1 and incycle[i] == 1:
                 dfs(i,0)
-        return ans
+        return ans if ans else -1
+                
+                
+            
+        
+        
+        
+            
+        
+                
+        
         
         
