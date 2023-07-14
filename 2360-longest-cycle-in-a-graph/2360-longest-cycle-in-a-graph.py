c409c1710baab1dfb2bd2019be06745d42dd3572
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-07-13 20:49:18 -0800

    Time: 1170 ms (76.69%), Space: 30.7 MB (87.43%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -1,6 +1,5 @@
 class Solution:
     def longestCycle(self, edges: List[int]) -> int:
-        incycle = [1 for i in range(len(edges))]
         degree = [0 for i in range(len(edges))]
         que = deque()
         visited = [0 for i in range(len(edges))]
@@ -15,25 +14,14 @@ class Solution:
      
         while que:
             now = que.popleft()
-            incycle[now] = 0
+            visited[now] = 1
             if edges[now] != -1:
                 degree[edges[now]] -= 1
                 if degree[edges[now]] == 0:
                     que.append(edges[now])
-       
-        def dfs(node,count):
-            nonlocal ans
-            if visited[node] == 1:
-                ans = max(ans,count)
-                return 
-            visited[node] = 1
-            if edges[node] != -1:
-                dfs(edges[node],count + 1)
-            
-            return
         
         for i in range(len(edges)):
-            if visited[i] != 1 and incycle[i] == 1:
+            if visited[i] != 1:
                 count = 1
                 n = i
                 while  edges[n] != i:
