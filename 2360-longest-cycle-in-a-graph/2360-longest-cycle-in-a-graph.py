5d39598d36143a2c29d8435a6fc4b81c2cc5eb3c
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-07-13 20:56:38 -0800

    Time: 1207 ms (73.46%), Space: 30.4 MB (93.02%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.py
@@ -2,7 +2,6 @@ class Solution:
     def longestCycle(self, edges: List[int]) -> int:
         degree = [0 for i in range(len(edges))]
         que = deque()
-        visited = [0 for i in range(len(edges))]
         ans = 0
         for i in range(len(edges)):
             if edges[i]!= -1:
@@ -14,19 +13,17 @@ class Solution:
      
         while que:
             now = que.popleft()
-            visited[now] = 1
             if edges[now] != -1:
                 degree[edges[now]] -= 1
                 if degree[edges[now]] == 0:
                     que.append(edges[now])
         
         for i in range(len(edges)):
-            if visited[i] != 1:
-                count = 1
+                count = 0
                 n = i
-                while  edges[n] != i:
+                while  degree[n] != 0:
                     count += 1
-                    visited[n] = 1
+                    degree[n] -= 1
                     n = edges[n]
                 ans = max(ans,count)
         return ans if ans else -1
