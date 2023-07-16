1906fb46c05d7ccd177d9ebd79f67a572b3d3b06
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-07-15 16:47:28 -0800

    Time: 167 ms (18.98%), Space: 16.5 MB (57.51%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.py
+++ 909-snakes-and-ladders/909-snakes-and-ladders.py
@@ -13,38 +13,38 @@ class Solution:
         for row in range(n):
             for col in range(n):
                 dic[mat[row][col]] = (row,col)
-    
+                
+        
         def dire(k):
             nonlocal n
             return [k + 1,min(k + 6,n**2)]
         
         que = deque()
-        que.append([1,0,0])
+        que.append(1)
         level = 0
         visited = set()
-        visited.add((1,0,0))
-        res = inf
+        visited.add(1)
         while que:
-               
-                val,lad,ans = que.popleft()
-                row = dic[val][0]
-                col = dic[val][1]
-                if val == n * n:
-                    res = min(ans,res)
-                if board[row][col] == -1 or lad == 1:
-                    neigh = dire(val)
-                    for x in range(neigh[0],neigh[1] + 1):
-                        if (x,0,ans + 1) not in visited:
-                            if ans <= n ** 2:
-                                que.append([x,0,ans + 1])
-                                visited.add((x,0,ans + 1))
-                else:
-                    if (board[row][col] ,1,ans) not in visited:
-                        if ans <= n:
-                            que.append([board[row][col],1,ans])
-                            visited.add((board[row][col],1,ans))
-        
-        return res if res != inf else -1
-                    
+            size = len(que)
+            for i in range(size):
+                cur = que.popleft()
+                if cur == n * n:
+                    return level
+                for i in range(1,7):
+                    if cur + i <= n**2:
+                        neigh = dic[cur + i]
+                        row = neigh[0]
+                        col = neigh[1]
+                        if board[row][col] == -1:
+                            if cur + i not in visited:
+                                que.append(cur + i)
+                                visited.add(cur + i)
+                        else:
+                            if board[row][col] not in visited:
+                                que.append(board[row][col])
+                                visited.add(board[row][col])
+            level += 1
+        return -1
                 
+                        
             
