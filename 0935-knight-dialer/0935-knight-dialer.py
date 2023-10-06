33cb5b8e0bce7e00f5ce4ca178e12a8a4cb55519
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-10-06 00:54:31 -0800

    Time: 772 ms (69.43%), Space: 16.2 MB (98.26%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -4,13 +4,12 @@ class Solution:
         dic = {0 : [4,6],1 : [8,6],2 : [7,9], 3 : [4,8],4 : [0,3,9],
                5 : [], 6 : [0,1,7], 7 : [2,6], 8 : [1,3], 9 : [4,2]}
         
-        dp = [[0] * 10 for i in range(n)]
-        for i in range(10):
-            dp[0][i] = 1
-        
-        for i in range(len(dp) - 1):
+        dp = [1] * 10
+        for i in range(n - 1):
+            newarr = [0 ] * 10
             for j in range(10):
                 for k in dic[j]:
-                    dp[i + 1][k] += dp[i][j]
-                    dp[i + 1][k] %= (10**9 + 7)
-        return sum(dp[-1]) % (10 ** 9 + 7)              
+                    newarr[k] += dp[j]
+                    newarr[k] %= (10**9 + 7)
+            dp = newarr
+        return sum(dp) % (10 ** 9 + 7)
