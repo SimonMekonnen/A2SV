7c5d910f0fde6b26d82dabeed72c0dbd38997212
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-10-08 11:03:15 -0800

    Time: 48 ms (5.51%), Space: 16.1 MB (99.91%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
@@ -6,40 +6,14 @@
 #         self.right = right
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        if not root1 and not root2:
-            return True
-        if not root1 or not root2:
-            return False
-        if root1.val != root2.val:
-            return False
-        def dfs(root1,root2,parent):
-            if not root1 and not root2:
-                return 
-            if parent != -1:
-                left = parent.left
-                right = parent.right
-            if not root1 or not root2 or root1.val != root2.val:
-                c = parent.left
-                parent.left = right
-                parent.right=  c
-            if root1.left:
-                if root2 == None:
-                    return
-                l = dfs(root1.left,root2.left,root1)
-            if root1.right:
-                if root2 == None:
-                    return
-                r = dfs(root1.right,root2.right,root1)
-        for i in range(100):
-            dfs(root1,root2,-1)
         
         def dfs(root1,root2):
                 if not root1 and not root2:
                     return True
-
+                
                 if not root1 or not root2 or root1.val != root2.val:
                     return False
-                return dfs(root1.left,root2.left) and dfs(root1.right,root2.right)
+                return (dfs(root1.left,root2.left) and dfs(root1.right,root2.right)) or (dfs(root1.right,root2.left) and dfs(root1.left,root2.right))
 
         return dfs(root1,root2)
         
