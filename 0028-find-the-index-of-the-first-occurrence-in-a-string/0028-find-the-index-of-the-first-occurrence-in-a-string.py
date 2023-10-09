eeb395d63d85b2f079a7fbdfa3537f997f8aa1be
SimonMekonnen <96548471+SimonMekonnen@users.noreply.github.com>
2023-10-08 23:40:59 -0800

    Time: 39 ms (50.57%), Space: 16.1 MB (96.72%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,17 +1,30 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        i = 0
-        j = 0
         
-        while i < len(haystack):   
-            if haystack[i] != needle[j]:
-                i = i - j + 1
-                j = 0
-    
+        need = 0
+        for i in needle:
+            need *= 27
+            need %= (10 ** 9 + 7)
+            need += ord(i) - ord('a') + 1
+            
+        
+        left = 0
+        window = 0
+        for right in range(len(haystack)):
+            if right < len(needle):
+                window *= 27
+                window %= (10 ** 9 + 7)
+                window += ord(haystack[right]) - ord('a') + 1
             else:
-                j += 1
-                if j == len(needle):
-                    return i - j + 1
-                i += 1
-        
-        return -1
+                if window == need:
+                    return left
+                window -= (ord(haystack[left]) - ord('a') + 1) * pow(27,right - left - 1,10**9 + 7)
+                window *= 27
+                window %= (10 ** 9 + 7)
+                window += ord(haystack[right]) - ord('a') + 1
+                left += 1
+                
+        return left if window == need else -1
+                
+            
+       
